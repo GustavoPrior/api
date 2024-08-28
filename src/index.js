a0f2b3a26eed4a0ff9c85bd951368a591ed818bf
GustavoPrior <gustavopprior1987@gmail.com>
2024-08-28 14:16:33 -0300

    add

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
-const express = require("express"); //importa o módulo Express
+const express = require("express");
 
-//Define uma classe para organizar a lógica da aplicação
 class AppController {
   constructor() {
-    //Cria uma nova instância do Express dentro da classe
     this.express = express();
-    //Chama o método middlewares para configurar os middlewares
     this.middlewares();
-    //Chama o método routes para definir as rotas de Api
     this.routes();
   }
   middlewares() {
-    //Permitir que a aplicção receba dados em formato JSON nas requisições
     this.express.use(express.json());
   }
 
-  //Define as rotas da nossa API 
   routes() {
-    //Define uma rota GET para o caminho health
+    const users = [];
+    this.express.post("/users",(req,res)=>{
+      const {id, nome, email, senha} = req.body;
+      users.push({id,nome,email,senha});
+      res.status(200).send({message:"Usuário cadastrado com sucesso"});
+    });
+
+    this.express.post("/auth",(req,res)=>{
+        const {email, senha} = req.body;
+        const auth = users.find((auth) => auth.email == email && auth.senha == senha);
+        if(auth){
+           res.status(200).send({message:"Usuário autenticado com sucesso"});
+        }
+        else{
+            res.status(400).send({message: "Usuário e senha nao encontrado" });
+        }
+      });
+  
+    this.express.get("/users/:id",(req,res)=>{
+      const{id} = req.params;
+      const user = users.find(user => user.id == id);
+      if(user){
+        res.status(200).send(user);
+      }
+      else{
+        res.status(400).send({message: "Usuário nao encontrado" });
+      }
+    });
+
+
+
     this.express.get("/health/", (req, res) => {
       res.send({ nome: "Gustavo Pereira Prior",
        idade:"17",
        cpf:"04883877523" });
-    });//Essa rota é usada para verificar se a API está OK
+    });
   }
 }
 
-//Exportando a instância de Express configurada, para que seja acessada em outros arquivos
+
 module.exports = new AppController().express;
